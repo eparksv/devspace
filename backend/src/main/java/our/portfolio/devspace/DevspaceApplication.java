a370053f3b82d477762fc4747f5f237338442bec
E. Park <epark.grace@gmail.com>
2022-07-04 21:25:24 +0900

    chore: 백엔드 스프링부트 프로젝트 생성

---
+++ backend/src/main/java/our/portfolio/devspace/DevspaceApplication.java
@@ -0,0 +1,13 @@
+package our.portfolio.devspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevspaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/our/portfolio/devspace/DevspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package our.portfolio.devspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
