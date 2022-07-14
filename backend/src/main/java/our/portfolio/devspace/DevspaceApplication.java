b49d0f086bb1838c43a8edfa9967bfa907d4c60a
E. Park <epark.grace@gmail.com>
2022-07-14 23:41:11 +0900

    style: 구글 스타일 가이드를 변형한 코딩 컨벤션 적용

--- backend/src/main/java/our/portfolio/devspace/DevspaceApplication.java
+++ backend/src/main/java/our/portfolio/devspace/DevspaceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevspaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevspaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevspaceApplication.class, args);
+    }
 
 }
--- backend/src/main/java/our/portfolio/devspace/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/our/portfolio/devspace/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package our.portfolio.devspace.configuration;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class WebSecurityConfiguration {
+
     @Value("${in-memory-user.name}")
     private String adminName;
 
@@ -23,9 +24,9 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .mvcMatchers("/docs/**", "/swagger-ui/**", "/v3/api-docs/**").hasRole("ADMIN")
-                .mvcMatchers("/api/hello").authenticated()
-                .anyRequest().denyAll()
+            .mvcMatchers("/docs/**", "/swagger-ui/**", "/v3/api-docs/**").hasRole("ADMIN")
+            .mvcMatchers("/api/hello").authenticated()
+            .anyRequest().denyAll()
         ).formLogin(withDefaults()).oauth2Login(withDefaults());
         return http.build();
     }
@@ -33,7 +34,7 @@ public class WebSecurityConfiguration {
     @Bean
     public UserDetailsService users() {
         UserDetails admin = User.withDefaultPasswordEncoder()
-                .username(adminName).password(adminPassword).roles("ADMIN").build();
+            .username(adminName).password(adminPassword).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(admin);
     }
 
