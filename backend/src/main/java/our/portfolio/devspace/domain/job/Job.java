a62630aa1ab75e3bdd848299e825a8ad964c37ed
E. Park <epark.grace@gmail.com>
2022-07-16 23:41:25 +0900

    feat: domain.user.User, domain.job.Job, domain.BaseTimeEntity 추가

---
+++ backend/src/main/java/our/portfolio/devspace/domain/job/Job.java
@@ -0,0 +1,38 @@
+package our.portfolio.devspace.domain.job;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import our.portfolio.devspace.domain.BaseTimeEntity;
+import our.portfolio.devspace.domain.user.User;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "jobs")
+public class Job extends BaseTimeEntity {
+
+    @OneToMany(mappedBy = "job")
+    final private List<User> users = new ArrayList<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 10)
+    private String title;
+
+    @Builder
+    public Job(String title) {
+        this.title = title;
+    }
+}
