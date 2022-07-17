6e9fef167b43600eb5fb2904740d746eebd4e664
E. Park <epark.grace@gmail.com>
2022-07-18 01:21:33 +0900

    refact: 사용하지 않는 인메모리 유저 설정 삭제

--- backend/src/main/java/our/portfolio/devspace/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/our/portfolio/devspace/configuration/WebSecurityConfiguration.java
@@ -1,25 +1,13 @@
 package our.portfolio.devspace.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfiguration {
-
-    @Value("${in-memory-user.name}")
-    private String adminName;
-
-    @Value("${in-memory-user.password}")
-    private String adminPassword;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
