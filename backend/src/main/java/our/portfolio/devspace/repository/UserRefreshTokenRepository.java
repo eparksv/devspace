27727b72b96fcb5de1b0fb20600090d978a645fd
LAYER6AI <75304316+destitutor@users.noreply.github.com>
2022-07-19 18:51:00 +0900

    feat: 이메일로 리프레시 토큰을 검색하는 기능 추가

--- backend/src/main/java/our/portfolio/devspace/repository/UserRefreshTokenRepository.java
+++ backend/src/main/java/our/portfolio/devspace/repository/UserRefreshTokenRepository.java
@@ -1,7 +1,9 @@
 package our.portfolio.devspace.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import our.portfolio.devspace.domain.user.UserRefreshToken;
 
 public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+    Optional<UserRefreshToken> findByEmail(String email);
 }
