b97a0f7f8cf72c8bd561e216ce46dbe289d06cd7
E. Park <epark.grace@gmail.com>
2022-08-07 11:39:55 +0900

    fix: ProfileCreationDto의 Validation 및 Lombok Annotation 수정
    - name 필드 @Length에 min 추가
    - introduction 필드 @Length에 min 삭제
    - 클래스 @NoArgsConstructor 삭제
    - 모든 필드 final로 변경

--- backend/src/main/java/our/portfolio/devspace/domain/profile/dto/ProfileCreationDto.java
+++ backend/src/main/java/our/portfolio/devspace/domain/profile/dto/ProfileCreationDto.java
@@ -3,23 +3,21 @@ package our.portfolio.devspace.domain.profile.dto;
 import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import our.portfolio.devspace.domain.profile.entity.Profile;
 
 @Getter
-@NoArgsConstructor
 public class ProfileCreationDto {
 
     @NotNull(message = "이름을 입력하세요.")
-    @Length(max = 12, message = "이름은 12자 이하로 입력하세요.")
-    private String name;
+    @Length(min = 2, max = 12, message = "이름은 12자 이하로 입력하세요.")
+    private final String name;
 
-    @Length(min = 0, max = 16383, message = "자기소개는 16,383자 이하로 입력하세요.") // MYSQL TEXT 타입 최대 65,535 byte / 4 (utf-8 한글자당 최대 4byte)
-    private String introduction;
+    @Length(max = 16383, message = "자기소개는 16,383자 이하로 입력하세요.") // MYSQL TEXT 타입 최대 65,535 byte / 4 (utf-8 한글자당 최대 4byte)
+    private final String introduction;
 
     @NotNull
-    private Integer jobId;
+    private final Integer jobId;
 
     @Builder
     public ProfileCreationDto(String name, String introduction, Integer jobId) {
