955e9e81c26046fc27897b6bec559de425f264d2
E. Park <epark.grace@gmail.com>
2022-08-20 19:48:53 +0900

    feat: Job 엔티티에 직군을 분류하는 JobType 필드 추가

--- backend/src/main/java/our/portfolio/devspace/domain/job/entity/Job.java
+++ backend/src/main/java/our/portfolio/devspace/domain/job/entity/Job.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,8 +33,13 @@ public class Job {
     @Column(nullable = false, length = 10)
     private String title;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private JobType type;
+
     @Builder
-    public Job(String title) {
+    public Job(JobType type, String title) {
         this.title = title;
+        this.type = type;
     }
 }
