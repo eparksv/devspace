2c17a8ee9ab3916bbec484edf7de87732103b961
E. Park <epark.grace@gmail.com>
2022-08-20 19:51:07 +0900

    feat: 직군 목록을 조회하는 서비스 메소드 추가

--- backend/src/main/java/our/portfolio/devspace/domain/job/service/JobService.java
+++ backend/src/main/java/our/portfolio/devspace/domain/job/service/JobService.java
@@ -2,6 +2,8 @@ package our.portfolio.devspace.domain.job.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import our.portfolio.devspace.domain.job.dto.ListJobsResponse;
 import our.portfolio.devspace.domain.job.entity.Job;
 import our.portfolio.devspace.domain.job.repository.JobRepository;
 import our.portfolio.devspace.exception.CustomException;
@@ -12,6 +14,11 @@ import our.portfolio.devspace.exception.ErrorDetail;
 public class JobService {
 
     private final JobRepository jobRepository;
+
+    @Transactional(readOnly = true)
+    public ListJobsResponse listJobs() {
+        return ListJobsResponse.from(jobRepository.findAll());
+    }
 
     public Job getJobById(Integer id) {
         return jobRepository.findById(id).orElseThrow(() -> new CustomException("Job Id " + id + "에 해당하는 직군이 없습니다.", ErrorDetail.JOB_NOT_FOUND));
