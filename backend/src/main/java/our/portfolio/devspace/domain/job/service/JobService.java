041a7edc1e569c9f837f1577bd6c506727be9aef
E. Park <epark.grace@gmail.com>
2022-08-20 20:19:59 +0900

    refact: 직군 ID로 직군 조회 시 예외를 던지는 코드를 리포지토리로 이전

--- backend/src/main/java/our/portfolio/devspace/domain/job/service/JobService.java
+++ backend/src/main/java/our/portfolio/devspace/domain/job/service/JobService.java
@@ -6,8 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import our.portfolio.devspace.domain.job.dto.ListJobsResponse;
 import our.portfolio.devspace.domain.job.entity.Job;
 import our.portfolio.devspace.domain.job.repository.JobRepository;
-import our.portfolio.devspace.exception.CustomException;
-import our.portfolio.devspace.exception.ErrorDetail;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +19,6 @@ public class JobService {
     }
 
     public Job getJobById(Integer id) {
-        return jobRepository.findById(id).orElseThrow(() -> new CustomException("Job Id " + id + "에 해당하는 직군이 없습니다.", ErrorDetail.JOB_NOT_FOUND));
+        return jobRepository.findByIdOrThrow(id);
     }
 }
