398b4de2014519e67be401512936b118de80d106
E. Park <epark.grace@gmail.com>
2022-08-28 23:31:58 +0900

    refact: 컨트롤러에서 UserId를 매개변수로 받아오도록 변경

--- backend/src/main/java/our/portfolio/devspace/domain/post/controller/PostController.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/controller/PostController.java
@@ -1,6 +1,5 @@
 package our.portfolio.devspace.domain.post.controller;
 
-import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import our.portfolio.devspace.common.dto.HttpResponseBody;
+import our.portfolio.devspace.configuration.security.oauth.domain.UserId;
 import our.portfolio.devspace.domain.post.dto.PostCreationRequestDto;
 import our.portfolio.devspace.domain.post.dto.PostCreationResponseDto;
 import our.portfolio.devspace.domain.post.service.PostService;
@@ -20,8 +20,8 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/api/posts")
-    public ResponseEntity<HttpResponseBody<PostCreationResponseDto>> createPost(@RequestBody @Valid PostCreationRequestDto dto, Principal userPrincipal) {
-        PostCreationResponseDto responseDto = postService.createPost(Long.parseLong(userPrincipal.getName()), dto);
+    public ResponseEntity<HttpResponseBody<PostCreationResponseDto>> createPost(@RequestBody @Valid PostCreationRequestDto dto, @UserId Long userId) {
+        PostCreationResponseDto responseDto = postService.createPost(userId, dto);
 
         // HTTP Status Code: 201 Created, Response Body: { message, data: PostCreationResponse}
         HttpResponseBody<PostCreationResponseDto> body = new HttpResponseBody<>("등록되었습니다.", responseDto);
--- backend/src/main/java/our/portfolio/devspace/domain/profile/controller/ProfileController.java
+++ backend/src/main/java/our/portfolio/devspace/domain/profile/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package our.portfolio.devspace.domain.profile.controller;
 
-import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import our.portfolio.devspace.common.dto.HttpResponseBody;
+import our.portfolio.devspace.configuration.security.oauth.domain.UserId;
 import our.portfolio.devspace.domain.profile.dto.CreateProfileRequest;
 import our.portfolio.devspace.domain.profile.dto.CreateProfileResponse;
 import our.portfolio.devspace.domain.profile.service.ProfileService;
@@ -20,8 +20,8 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @PostMapping(value = "/api/profiles")
-    public ResponseEntity<HttpResponseBody<CreateProfileResponse>> createProfile(@RequestBody @Valid CreateProfileRequest requestDto, Principal userPrincipal) {
-        CreateProfileResponse responseDto = profileService.createProfile(Long.parseLong(userPrincipal.getName()), requestDto);
+    public ResponseEntity<HttpResponseBody<CreateProfileResponse>> createProfile(@RequestBody @Valid CreateProfileRequest requestDto, @UserId Long userId) {
+        CreateProfileResponse responseDto = profileService.createProfile(userId, requestDto);
 
         // HTTP Status Code: 201 Created, Response Body: { message, data: CreateProfileResponse}
         HttpResponseBody<CreateProfileResponse> body = new HttpResponseBody<>("프로필이 저장되었습니다.", responseDto);
