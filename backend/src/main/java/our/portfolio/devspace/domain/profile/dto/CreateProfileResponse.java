815baa491030cecb4121ad0b086a85102385e371
E. Park <epark.grace@gmail.com>
2022-08-31 13:42:37 +0900

    refact: Profile DTO의 매핑 메소드 삭제

--- backend/src/main/java/our/portfolio/devspace/domain/profile/dto/CreateProfileResponse.java
+++ backend/src/main/java/our/portfolio/devspace/domain/profile/dto/CreateProfileResponse.java
@@ -2,15 +2,10 @@ package our.portfolio.devspace.domain.profile.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import our.portfolio.devspace.domain.profile.entity.Profile;
 
 @Getter
 @AllArgsConstructor
 public class CreateProfileResponse {
 
     private final Long id;
-
-    public static CreateProfileResponse from(Profile profile) {
-        return new CreateProfileResponse(profile.getId());
-    }
 }
