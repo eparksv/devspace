efd66d52efa8e106b723630f556a090a8e040448
E. Park <epark.grace@gmail.com>
2022-08-31 12:20:23 +0900

    refact: Entity ID를 Entity로 매핑하는 mapstruct mapper 추가

---
+++ backend/src/main/java/our/portfolio/devspace/common/mapper/EntityMapper.java
@@ -0,0 +1,24 @@
+package our.portfolio.devspace.common.mapper;
+
+import org.mapstruct.TargetType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.web.context.WebApplicationContext;
+
+public class EntityMapper {
+
+    private final Repositories repositories;
+
+    public EntityMapper(WebApplicationContext context) {
+        this.repositories = new Repositories(context);
+    }
+
+    private <T, N extends Number> JpaRepository<T, N> getRepository(Class<T> entityClass) {
+        return (JpaRepository<T, N>) repositories.getRepositoryFor(entityClass).orElseThrow(IllegalArgumentException::new);
+    }
+
+    @IdToEntity
+    public <T, N extends Number> T resolve(N id, @TargetType Class<T> entityClass) {
+        return getRepository(entityClass).findById(id).orElseThrow();
+    }
+}
