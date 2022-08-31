6f855da15ad941911e18fe66e5b9eb4af8a13307
E. Park <epark.grace@gmail.com>
2022-08-31 12:22:43 +0900

    refact: Profile 생성 DTO와 Profile Entity를 매핑하는 mapstruct mapper 추가

---
+++ backend/src/main/java/our/portfolio/devspace/common/mapper/ProfileMapper.java
@@ -0,0 +1,17 @@
+package our.portfolio.devspace.common.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import our.portfolio.devspace.domain.profile.dto.CreateProfileRequest;
+import our.portfolio.devspace.domain.profile.dto.CreateProfileResponse;
+import our.portfolio.devspace.domain.profile.entity.Profile;
+
+@Mapper(uses = {EntityMapper.class})
+public interface ProfileMapper {
+
+    @Mapping(source = "userId", target = "user", qualifiedBy = {IdToEntity.class})
+    @Mapping(source = "dto.jobId", target = "job", qualifiedBy = {IdToEntity.class})
+    Profile toEntity(Long userId, CreateProfileRequest dto);
+
+    CreateProfileResponse toCreateProfileResponse(Profile profile);
+}
