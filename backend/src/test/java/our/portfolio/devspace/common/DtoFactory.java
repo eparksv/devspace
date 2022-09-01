2a43ae5f1ca39c3d1989045cdcec9b07429801f2
E. Park <epark.grace@gmail.com>
2022-09-01 22:02:11 +0900

    test: post mapper 테스트 코드 추가

---
+++ backend/src/test/java/our/portfolio/devspace/common/DtoFactory.java
@@ -0,0 +1,30 @@
+package our.portfolio.devspace.common;
+
+import java.util.List;
+import our.portfolio.devspace.domain.post.dto.PostCreationRequestDto;
+import our.portfolio.devspace.domain.profile.dto.CreateProfileRequest;
+import our.portfolio.devspace.domain.profile.dto.CreateProfileRequest.ReferenceLinkDto;
+
+public class DtoFactory {
+    public static PostCreationRequestDto postCreationRequestDto() {
+        return PostCreationRequestDto.builder()
+            .title("제목")
+            .content("본문")
+            .secret(false)
+            .hashtags(List.of("#태그1", "#태그2", "#태그3"))
+            .build();
+    }
+
+    public static CreateProfileRequest createProfileRequest() {
+        return CreateProfileRequest.builder()
+            .company("회사")
+            .introduction("자기소개")
+            .jobId(1)
+            .career("1년")
+            .name("이름")
+            .referenceLinks(List.of(
+                new ReferenceLinkDto("구글", "https://www.google.com"),
+                new ReferenceLinkDto("깃허브", "https://www.github.com")))
+            .build();
+    }
+}
