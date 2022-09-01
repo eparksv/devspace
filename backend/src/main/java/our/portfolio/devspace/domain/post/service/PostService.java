956844076d0b86e6edb04662c625b444ce2d6657
E. Park <epark.grace@gmail.com>
2022-09-01 12:01:41 +0900

    refact: Post 생성 DTO와 Entity의 매핑에 mapper를 사용하도록 변경

--- backend/src/main/java/our/portfolio/devspace/domain/post/service/PostService.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/service/PostService.java
@@ -1,6 +1,5 @@
 package our.portfolio.devspace.domain.post.service;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,10 +7,7 @@ import our.portfolio.devspace.common.mapper.PostMapper;
 import our.portfolio.devspace.domain.post.dto.PostCreationRequestDto;
 import our.portfolio.devspace.domain.post.dto.PostCreationResponseDto;
 import our.portfolio.devspace.domain.post.entity.Post;
-import our.portfolio.devspace.domain.post.entity.PostHashtag;
 import our.portfolio.devspace.domain.post.repository.PostRepository;
-import our.portfolio.devspace.domain.profile.entity.Profile;
-import our.portfolio.devspace.domain.profile.service.ProfileService;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +15,10 @@ import our.portfolio.devspace.domain.profile.service.ProfileService;
 public class PostService {
 
     private final PostMapper postMapper;
-    private final ProfileService profileService;
-    private final HashtagService hashtagService;
     private final PostRepository postRepository;
 
     public PostCreationResponseDto createPost(Long userId, PostCreationRequestDto dto) {
-        Profile profile = profileService.getProfileById(userId);
-        // TODO hashtags null 체크
-        List<PostHashtag> hashtags = hashtagService.getHashtagsOfPost(dto.getHashtags());
-        Post post = dto.toEntity(profile, hashtags);
+        Post post = postMapper.toEntity(userId, dto);
 
         // 저장된 Post를 DTO로 변환하여 반환한다.
         return postMapper.toPostCreationResponseDto(postRepository.save(post));
