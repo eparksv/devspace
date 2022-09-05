9918f61d1e68ac0c159a30df8e9d62bb678fa13f
E. Park <epark.grace@gmail.com>
2022-09-06 01:09:07 +0900

    feat: 직군 목록 조회 API가 직군 종류를 Path Variable로 받도록 변경

--- backend/src/main/java/our/portfolio/devspace/domain/job/controller/JobController.java
+++ backend/src/main/java/our/portfolio/devspace/domain/job/controller/JobController.java
@@ -1,12 +1,14 @@
 package our.portfolio.devspace.domain.job.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import our.portfolio.devspace.common.dto.HttpResponseBody;
-import our.portfolio.devspace.domain.job.dto.ListJobsResponse;
+import our.portfolio.devspace.domain.job.dto.JobResponse;
 import our.portfolio.devspace.domain.job.service.JobService;
 
 @RequiredArgsConstructor
@@ -14,9 +16,9 @@ import our.portfolio.devspace.domain.job.service.JobService;
 public class JobController {
     private final JobService jobService;
 
-    @GetMapping("/api/jobs")
-    public ResponseEntity<HttpResponseBody<ListJobsResponse>> listJobs() {
-        HttpResponseBody<ListJobsResponse> body = new HttpResponseBody<>("직군 목록이 조회되었습니다.", jobService.listJobs());
+    @GetMapping("/api/jobs/{type}")
+    public ResponseEntity<HttpResponseBody<List<JobResponse>>> listJobs(@PathVariable String type) {
+        HttpResponseBody<List<JobResponse>> body = new HttpResponseBody<>("직군 목록이 조회되었습니다.", jobService.listJobs(type));
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
