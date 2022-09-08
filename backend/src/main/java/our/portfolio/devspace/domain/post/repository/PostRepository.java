47375301747577532cdeec5021e390b39a7e4b46
E. Park <epark.grace@gmail.com>
2022-09-09 01:35:47 +0900

    test: 포스팅의 id를 커서, secret을 조건으로 최신순 20개의 포스팅을 찾는 레포지토리 메소드 추가

--- backend/src/main/java/our/portfolio/devspace/domain/post/repository/PostRepository.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package our.portfolio.devspace.domain.post.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import our.portfolio.devspace.domain.post.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findFirst20ByIdLessThanAndSecret(Long id, Boolean secret, Sort sort);
 }
