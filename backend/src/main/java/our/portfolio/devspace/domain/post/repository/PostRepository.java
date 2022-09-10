d66401676fd37b5dc05607d1350bae123bb881a9
E. Park <epark.grace@gmail.com>
2022-09-10 10:11:26 +0900

    feat: 포스팅의 secret을 조건으로 최신순 20개의 포스팅을 찾는 레포지토리 메소드 추가

--- backend/src/main/java/our/portfolio/devspace/domain/post/repository/PostRepository.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/repository/PostRepository.java
@@ -7,5 +7,7 @@ import our.portfolio.devspace.domain.post.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findFirst20BySecret(Boolean secret, Sort sort);
+
     List<Post> findFirst20ByIdLessThanAndSecret(Long id, Boolean secret, Sort sort);
 }
