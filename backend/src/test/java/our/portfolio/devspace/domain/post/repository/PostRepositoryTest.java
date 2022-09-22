f578507eac58c755e3e45eff70e041c5772b92c1
E. Park <epark.grace@gmail.com>
2022-09-22 16:37:54 +0900

    test: PostRepository 쿼리메소드 변경을 테스트코드에 반영

--- backend/src/test/java/our/portfolio/devspace/domain/post/repository/PostRepositoryTest.java
+++ backend/src/test/java/our/portfolio/devspace/domain/post/repository/PostRepositoryTest.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Sort;
-import our.portfolio.devspace.domain.job.repository.JobRepository;
 import our.portfolio.devspace.domain.post.entity.Post;
-import our.portfolio.devspace.domain.user.repository.UserRepository;
 
 @DataJpaTest
 class PostRepositoryTest {
@@ -21,16 +19,16 @@ class PostRepositoryTest {
 
     @Test
     @DisplayName("포스팅의 PK가 id보다 작고 secret이 false인 포스팅을 PK 내림차순으로 20개 찾는다.")
-    void findFirst20ByIdLessThan() {
+    void findFirst21ByIdLessThan() {
         // ** Given **
         Long id = 50L;
         Sort sort = Sort.by("id").descending();
 
         // ** When **
-        List<Post> posts = postRepository.findFirst20ByIdLessThanAndSecret(id, false, sort);
+        List<Post> posts = postRepository.findFirst21ByIdLessThanAndSecret(id, false, sort);
 
         // ** Then **
-        assertThat(posts.size()).isEqualTo(20);
+        assertThat(posts.size()).isEqualTo(21);
         assertThat(posts.get(0).getId()).isLessThan(id);
         assertThat(posts).allMatch(post -> !post.getSecret());
         assertThat(posts).isSortedAccordingTo(Comparator.comparingLong(Post::getId).reversed());
@@ -38,15 +36,15 @@ class PostRepositoryTest {
 
     @Test
     @DisplayName("포스팅의 secret이 false인 포스팅을 PK 내림차순으로 20개 찾는다.")
-    void findFirst20BySecret() {
+    void findFirst21BySecret() {
         // ** Given **
         Sort sort = Sort.by("id").descending();
 
         // ** When **
-        List<Post> posts = postRepository.findFirst20BySecret(false, sort);
+        List<Post> posts = postRepository.findFirst21BySecret(false, sort);
 
         // ** Then **
-        assertThat(posts.size()).isEqualTo(20);
+        assertThat(posts.size()).isEqualTo(21);
         assertThat(posts).allMatch(post -> !post.getSecret());
         assertThat(posts).isSortedAccordingTo(Comparator.comparingLong(Post::getId).reversed());
     }
