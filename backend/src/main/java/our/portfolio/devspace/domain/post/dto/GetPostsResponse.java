75531bd0dfc1f947068496f85b566c5af28970a3
E. Park <epark.grace@gmail.com>
2022-09-22 16:40:44 +0900

    feat: GetPostResponse에서 hasNext 필드를 삭제
    - nextRequestUri의 null 체크로 다음 페이지 유무를 판별

--- backend/src/main/java/our/portfolio/devspace/domain/post/dto/GetPostsResponse.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/dto/GetPostsResponse.java
@@ -7,16 +7,14 @@ import lombok.Getter;
 @Getter
 public class GetPostsResponse {
 
-    private final int count;
+    private final Long count;
     private final List<PostPreviewResponse> posts;
-    private final boolean hasNext;
     private final String nextRequestUri;
 
     @Builder
-    public GetPostsResponse(int count, List<PostPreviewResponse> posts, boolean hasNext, String nextRequestUri) {
+    public GetPostsResponse(Long count, List<PostPreviewResponse> posts, String nextRequestUri) {
         this.count = count;
         this.posts = posts;
-        this.hasNext = hasNext;
         this.nextRequestUri = nextRequestUri;
     }
 }
