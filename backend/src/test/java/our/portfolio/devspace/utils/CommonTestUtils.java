f9705d6b9686e3216b3a75d35c25836571873ad3
E. Park <epark.grace@gmail.com>
2022-09-23 23:57:14 +0900

    test: 테스트코드 LocalDateTime의 직렬화를 위해 유틸의 ObjectMapper에 모듈 적용

--- backend/src/test/java/our/portfolio/devspace/utils/CommonTestUtils.java
+++ backend/src/test/java/our/portfolio/devspace/utils/CommonTestUtils.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 public class CommonTestUtils {
 
     public static String valueToString(Object value) throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(value);
+        return new ObjectMapper().findAndRegisterModules().writeValueAsString(value);
     }
 
     public static void setIdField(Object object, Number id) throws IllegalAccessException {
