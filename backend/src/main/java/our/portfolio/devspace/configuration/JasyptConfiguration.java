7aaf2b648184076e6cd46866192b76b6ca799995
ensk26 <tjensk26@gmail.com>
2022-10-01 21:31:11 +0900

    feat : jasypt 이용해 암호화 기능 추가

---
+++ backend/src/main/java/our/portfolio/devspace/configuration/JasyptConfiguration.java
@@ -0,0 +1,33 @@
+package our.portfolio.devspace.configuration;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfiguration {
+
+    @Value("${DEVSPACE_JASYPT_PASSWORD}")
+    String password;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 횟수
+        config.setPoolSize("1"); // instance pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setStringOutputType("base64"); // 인코딩 방식
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
