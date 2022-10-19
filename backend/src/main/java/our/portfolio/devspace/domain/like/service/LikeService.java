2bd622487fabaa8b7ee381a0e7b61ebbfc15e37b
ensk26 <tjensk26@gmail.com>
2022-10-20 03:12:19 +0900

    feat : LikeService 에서 최대 8개 데이터 반환 설정, 관련된 error 추가

--- backend/src/main/java/our/portfolio/devspace/domain/like/service/LikeService.java
+++ backend/src/main/java/our/portfolio/devspace/domain/like/service/LikeService.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import our.portfolio.devspace.domain.like.dto.GetLikeResponse;
 import our.portfolio.devspace.domain.like.repository.LikeRepository;
+import our.portfolio.devspace.exception.CustomException;
 
 import java.util.List;
+
+import static our.portfolio.devspace.exception.ErrorDetail.INVALID_PARAMETER_VALUE;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,9 @@ public class LikeService {
 
     public List<GetLikeResponse> listLikes(Long id, Pageable pageable) {
 
+        if (pageable.getPageSize() > 8) {
+            throw new CustomException(INVALID_PARAMETER_VALUE);
+        }
         return likeRepository.findLikeUserByPostId(id, pageable);
     }
 }
--- backend/src/main/java/our/portfolio/devspace/exception/ErrorDetail.java
+++ backend/src/main/java/our/portfolio/devspace/exception/ErrorDetail.java
@@ -16,7 +16,8 @@ public enum ErrorDetail {
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "Entity Not Found"),
     INVALID_INPUT_VALUE(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Input Value"),
     POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "Posts Not Found"),
-    PROFILE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Profile Already Exists");
+    PROFILE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Profile Already Exists"),
+    INVALID_PARAMETER_VALUE(HttpStatus.BAD_REQUEST, "Invalid Parameter Value");
 
     private final HttpStatus status;
     private final String description;
