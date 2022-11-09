2127bca01304b5d061efaa7bf61067edaf3b7b14
ensk26 <tjensk26@gmail.com>
2022-11-09 23:37:58 +0900

    feat: 좋아요한 사람을 생성해주는 controller
    createLike 메서드 생성 to LikeController
    - 좋아요한 사람을 저장하기위한 controller
    - postMapping
    - id 값을 url로 받아온다.
    - id 값으로 likeService의 cerateLike 호출

--- backend/src/main/java/our/portfolio/devspace/domain/like/controller/LikeController.java
+++ backend/src/main/java/our/portfolio/devspace/domain/like/controller/LikeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import our.portfolio.devspace.common.dto.HttpResponseBody;
 import our.portfolio.devspace.domain.like.dto.GetLikeResponse;
@@ -29,12 +30,16 @@ public class LikeController {
      * @return 결과 메시지와 {@link GetLikeResponse}의 List를 담은 {@link HttpResponseBody}, Status 200 OK
      */
     @GetMapping("api/like/{id}")
-    public ResponseEntity<HttpResponseBody<List<GetLikeResponse>>> listLikes(@PathVariable Long id,Pageable pageable) {
+    public ResponseEntity<HttpResponseBody<List<GetLikeResponse>>> listLikes(@PathVariable Long id, Pageable pageable) {
 
         HttpResponseBody<List<GetLikeResponse>> body
                 = new HttpResponseBody<>("좋아요 회원 목록이 조회되었습니다.", likeService.listLikes(id, pageable));
         return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
+    @PostMapping("api/like/{id}")
+    public void createLike(@PathVariable Long id) {
+        likeService.createLike(id);
+    }
 
 }
