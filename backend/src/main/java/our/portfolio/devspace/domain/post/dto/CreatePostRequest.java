6aa988a71d4e27ffd0078ded6cee4d45502d6bf0
ensk26 <tjensk26@gmail.com>
2022-11-29 00:50:17 +0900

    fix: InvalidDefinitionException 오류 해결
    InvalidDefinitionException 오류 발생
    - 기본 생성자를 생성 안해서 발생
    - CreatePostRequest, CreateProfileRequest 에 @NoArgsConstructor 추가, final 제거

--- backend/src/main/java/our/portfolio/devspace/domain/post/dto/CreatePostRequest.java
+++ backend/src/main/java/our/portfolio/devspace/domain/post/dto/CreatePostRequest.java
@@ -6,25 +6,27 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
+@NoArgsConstructor
 public class CreatePostRequest {
 
-    private final String title;
+    private String title;
 
     @NotBlank
     @Length(max = 16383, message = "본문 내용은 16,383자 이하로 입력하세요.") // MYSQL TEXT 타입 최대 65,535 byte / 4 (utf-8 한글자당 최대 4byte)
-    private final String content;
+    private String content;
 
     @NotNull
-    private final Boolean secret;
+    private Boolean secret;
 
     @Size(max = 3)
-    private final List<String> hashtags;
+    private List<String> hashtags;
 
     @NotNull
-    private final Integer categoryId;
+    private Integer categoryId;
 
     @Builder
     public CreatePostRequest(String title, String content, Boolean secret, List<String> hashtags, Integer categoryId) {
--- backend/src/main/java/our/portfolio/devspace/domain/profile/dto/CreateProfileRequest.java
+++ backend/src/main/java/our/portfolio/devspace/domain/profile/dto/CreateProfileRequest.java
@@ -10,35 +10,37 @@ import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 @Getter
+@NoArgsConstructor
 public class CreateProfileRequest {
 
     @NotNull(message = "이름을 입력하세요.")
     @Length(min = 2, max = 12, message = "이름은 2자 이상, 12자 이하로 입력하세요.")
     @Pattern(regexp = "^[A-Z가-힣]+$", flags = Flag.CASE_INSENSITIVE)
-    private final String name;
+    private String name;
 
     @NotBlank(message = "자기소개를 입력하세요.")
     @Length(max = 255, message = "자기소개는 255자 이하로 입력하세요.")
-    private final String introduction;
+    private String introduction;
 
     @NotNull(message = "직군을 선택하세요.")
-    private final Integer jobId;
+    private Integer jobId;
 
     @Length(max = 30, message = "회사명은 30자 이하로 입력하세요.")
     @Pattern(regexp = "^[A-Z가-힣0-9]+$", flags = Flag.CASE_INSENSITIVE)
-    private final String company;
+    private String company;
 
     @Length(max = 30, message = "경력 기간은 30자 이하로 입력하세요.")
     @Pattern(regexp = "^[A-Z가-힣0-9]+$", flags = Flag.CASE_INSENSITIVE)
-    private final String career;
+    private String career;
 
     @Valid
     @Size(max = 5, message = "링크는 5개 이하로 입력하세요.")
-    private final List<ReferenceLinkDto> referenceLinks;
+    private List<ReferenceLinkDto> referenceLinks;
 
     @Builder
     public CreateProfileRequest(String name, String introduction, Integer jobId, String company, String career, List<ReferenceLinkDto> referenceLinks) {
@@ -52,6 +54,7 @@ public class CreateProfileRequest {
 
     @Getter
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class ReferenceLinkDto {
 
         @NotNull(message = "링크 이름을 입력하세요.")
