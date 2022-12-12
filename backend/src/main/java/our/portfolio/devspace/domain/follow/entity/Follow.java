9a1884d286f2784d36875b2b3b62dd286a45c444
ensk26 <tjensk26@gmail.com>
2022-12-12 23:31:29 +0900

    feat: follow entity 추가
    - 팔로우, 팔로잉 기능 객체 생성
    - user와 다대일 관계

---
+++ backend/src/main/java/our/portfolio/devspace/domain/follow/entity/Follow.java
@@ -0,0 +1,26 @@
+package our.portfolio.devspace.domain.follow.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import our.portfolio.devspace.domain.user.entity.User;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followee_id", nullable = false)
+    private User followee;
+}
