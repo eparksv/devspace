0e90576de2211b554e0edc25b16a7b7c261e89dd
jass--ss <jsmsue010@gmail.com>
2023-02-09 02:03:18 +0900

    feat 로그인 유무 체크

--- frontend/component/develop/Develop.page.tsx
+++ frontend/component/develop/Develop.page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledDev } from './Develop_style';
 
 function Develop() {
-	return <StyledDev>Develop</StyledDev>;
+	return <StyledDev className='develop'>Develop</StyledDev>;
 }
 
 export default Develop;
--- frontend/component/develop/Develop_style.tsx
+++ frontend/component/develop/Develop_style.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const StyledDev = styled.section`
 	position: relative;
-	width: 100%;
+	width: calc(100% - 338px);
 	min-height: 100vh;
 	margin-top: 4rem;
 	border: 4px solid blue;
